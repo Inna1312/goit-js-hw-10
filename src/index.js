e34b4c3fca65405cdbc002ed2d48856dc4841bec
Inna1312 <126244200+Inna1312@users.noreply.github.com>
2023-08-10 16:10:06 +0300

    start

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// import axios from 'axios';
+
+// const API_KEY = 'live_zqqDDCu2mqHhx1IhRetN0pMw3AfY1jOo9j4aM5HkerJ1bBKBFXVa7G2GFINWn9xu';
+// axios.defaults.headers.common['x-api-key'] = API_KEY;
+// const BASE_URL = 'https://api.thecatapi.com/v1';
